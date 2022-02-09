04b96588e43f2dcd21a5980c8480460372685369
Fazal <fazalsultan88@gmail.com>
2022-02-09 23:52:06 +0530

    Initial commit

---
+++ app/src/main/java/com/androidvoyage/zakat/ZakatApp.kt
@@ -0,0 +1,10 @@
+package com.androidvoyage.zakat
+
+import android.app.Application
+
+/**
+ * Created by Fazal on 09/02/22.
+ * Copyright (c) 2022 PayNearby. All rights reserved.
+ */
+class ZakatApp : Application() {
+}
